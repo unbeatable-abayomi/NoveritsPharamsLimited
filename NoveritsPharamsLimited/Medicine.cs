22dc5a7c2308b9f2dc2d3773a17951c115627376
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-27 19:37:22 +0100

    Add project files.

---
+++ NoveritsPharamsLimited/Medicine.cs
@@ -0,0 +1,78 @@
+﻿namespace NoveritsPharamsLimited
+{
+    class Medicine
+    {
+
+        private string mediCode;
+        private string mediName;
+        private string manuName;
+        private double unitPrc;
+        private int quanty;
+        private string batchNum;
+        //private string Maundate;
+        //private string expriydate;
+
+        public Medicine(string mediCde, string mediNme, string mauNme, double untPrc, int qunt, string batNum)
+        {
+            MedicineCode = mediCde;
+            MedicineName = mediNme;
+            ManufactuerName = mauNme;
+            UnitPrice = untPrc;
+            Quantity = qunt;
+            BatchNumber = batNum;
+
+
+
+        }
+
+        public string MedicineCode
+        {
+            get { return mediCode; }
+            private set { mediCode = value; }
+        }
+        public string MedicineName
+        {
+            get { return mediName; }
+            private set { mediName = value; }
+        }
+        public string ManufactuerName
+        {
+            get { return manuName; }
+            private set { manuName = value; }
+        }
+
+        public double UnitPrice
+        {
+            get { return unitPrc; }
+            private set
+            {
+                if (value > 0.0)
+                {
+                    unitPrc = value;
+                }
+            }
+        }
+        public int Quantity
+        {
+            get { return quanty; }
+            private set
+            {
+                if (value > 0)
+                {
+                    quanty = value;
+                }
+            }
+        }
+
+        public string BatchNumber
+        {
+            get { return batchNum; }
+            private set
+            {
+                batchNum = value;
+            }
+        }
+
+
+    }
+}
---
+++ NoveritsPharamsLimited/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoveritsPharamsLimited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
